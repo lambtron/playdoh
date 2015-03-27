da51ad453a7244ad71c4b92a893bf4402e3d658f
Ian Storm Taylor <ian@ianstormtaylor.com>
2015-03-27 02:53:32 -0700

    swap to left nav

--- support/index.js
+++ support/index.js
@@ -143,10 +143,12 @@ var links = document.querySelector('.Summary-link');
 
 analytics.trackLink(links, 'Clicked Article Link', function(el){
   return {
-    name: el.textContent,
-    date: el.parentNode.parentNode.querySelector('.Summary-date').getAttribute('datetime'),
-    href: el.href,
-    external: el.classList.contains('Summary-external-link')
+    article: {
+      name: el.textContent,
+      date: el.parentNode.parentNode.querySelector('.Summary-date').getAttribute('datetime'),
+      href: el.href,
+      external: el.classList.contains('Summary-external-link')
+    }
   };
 });
 
